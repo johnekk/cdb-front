56e8c1d1c80d7828d0ffd07e499cd2ad5c8ca0a9
loic holleville <loic.holleville@viacesi.fr>
2020-01-29 10:25:07 +0100

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { ComputerListComponent } from './computer-list/computer-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'computers',
+    component: ComputerListComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'computers',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
