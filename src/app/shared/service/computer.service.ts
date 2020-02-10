84d6d6475f21a25ec6a4a9553957868eaf27da4c
Mjouhri.jouhri <mjouhri.mjouhri@excilys.com>
2020-02-10 13:55:48 +0100

    upgrade new computer : list companies & validator date

--- src/app/shared/service/computer.service.ts
+++ src/app/shared/service/computer.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Computer } from '../model/computer.model';
 import { ComputerDTO } from '../model/computerDTO.model';
+import { Company } from '../model/company.model';
+import { CompanyDTO } from '../model/companyDTO.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,7 @@ export class ComputerService {
 
   public ip = '10.0.1.76';
   public url = 'http://' + this.ip + ':8080/computer-database/computers';
+  public urlCompanies = 'http://' + this.ip + ':8080/computer-database/companys';
 
   constructor(private readonly httpClient: HttpClient) { }
 
@@ -19,12 +22,16 @@ export class ComputerService {
       return this.httpClient.get<Computer[]>(this.url);
   }
 
+  getCompanies(): Observable<CompanyDTO[]> {
+    return this.httpClient.get<CompanyDTO[]>(this.urlCompanies);
+}
+
   newComputer(computer: ComputerDTO):Observable<ComputerDTO>{
     return this.httpClient.post<ComputerDTO>(this.url, computer)
   }
 
   deleteComputer(id: number): Observable<any> {
     return this.httpClient.delete(`${this.url}/${id}/`);
-}
+  }
 
 }
